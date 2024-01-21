7815bf3c1499733299622f3e2df737a3c575602c
pankusingh02 <pankajaswal2020@gmail.com>
2024-01-21 19:24:17 +0530

    react STATE and useState hook

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,15 +1,46 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
+  const [step, setSteps] = useState(1);
+
+  function handlePrevious() {
+    if(step>1)
+    setSteps(step - 1);
+  }
+  function handleNext() {
+    if(step<3)
+    setSteps(step + 1);
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
-      <p className="message"> </p>
-      <div>
-        <button>Previous</button>
-        <button>Next</button>
+      <p className="message">
+        step {step} : {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
