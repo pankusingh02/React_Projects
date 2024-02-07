8bf25abe73d847b0c4d627683d92ff8384178ced
pankusingh02 <pankajaswal2020@gmail.com>
2024-02-07 14:26:12 +0530

    Intialising and adding some dummy data

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList initialFriends={initialFriends} />
+      </div>
     </div>
   );
 }
 
-export default App;
+function FriendsList({ initialFriends }) {
+  const friends = initialFriends;
+  return friends.map((friend) => (
+    <div>
+      {" "}
+      <Friend friend={friend} />
+    </div>
+  ));
+}
+
+function Friend({ friend }) {
+  return <li>{friend}</li>;
+}
