8d425ae9bdffe7afcba16a4a864e8aad679d7c7a
pankusingh02 <pankajaswal2020@gmail.com>
2024-02-08 13:18:52 +0530

    Added form and button componet

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -24,7 +24,10 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList initialFriends={initialFriends} />
+        <FormAddFriend />
+        <Button>Add friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -40,5 +43,66 @@ function FriendsList({ initialFriends }) {
 }
 
 function Friend({ friend }) {
-  return <li>{friend}</li>;
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name}
+          {Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          You {friend.name} ows you
+          {Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && (
+        <p>
+          You and {friend.name}
+          are even
+        </p>
+      )}
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🧑‍🤝‍🧑 Friend name</label>
+      <input type="text" />
+
+      <label>🌆 Image URL</label>
+      <input type="text" />
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Spilit a bill with x</h2>
+      <label>💰Bill Value</label>
+      <input type="text" />
+      <label>🧑Your Expense</label>
+      <input type="text" />
+      <label>🧑‍🤝‍🧑X's Expense</label>
+      <input type="text" disabled />
+      <label>💱Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">x</option>
+      </select>
+      <Button>Spilit bill</Button>
+    </form>
+  );
 }
