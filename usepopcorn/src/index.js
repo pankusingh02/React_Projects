9b560ba5703321e3ad3f16d808ef1f2049baad28
Pankaj Singh <pankajaswal2020@gmail.com>
2024-02-15 01:25:15 +0530

    improving reusability with props

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import StarRating from "./starRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        size={24}
+        color="orange"
+        maxRating={10}
+        onSetRating={setMovieRating}
+      />
+      <h2>This movie was rated {movieRating} </h2>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      size={24}
+      messages={["Terriable", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={4}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
--- usepopcorn/src/starRating.js
+++ usepopcorn/src/starRating.js
@@ -32,21 +32,30 @@ const starContainerStyle = {
   display: "flex",
 };
 
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = "48",
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) {
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
   const textStyle = {
     lineHeight: "1",
     margin: "0",
-    // fontSize: `${size / 1.5}px`,
+    fontSize: `${size / 1.5}px`,
   };
 
-  // function handleRating() {
-  //   setRating(rating);
-  // }
+  function handleRating(rating) {
+    setRating(rating);
+    onSetRating(rating);
+  }
 
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
@@ -55,39 +64,45 @@ export default function StarRating({ maxRating = 5 }) {
               tempRating ? tempRating >= i + 1 : rating >= i + 1
             } /**rating will get set using the setRating , then every element will check wheather it is 
             greater then rating or not. if it is greater then rating then the "full" value willbe "true" otherwise "false" */
-            onClick={() => setRating(i + 1)}
+            onClick={() => handleRating(i + 1)}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
 
-const starStyle = {
-  width: "48px",
-  height: "48px",
-  display: "block",
-  cursor: "pointer",
-};
+function Star({ onClick, full, onHoverIn, onHoverOut, color, size }) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: "block",
+    cursor: "pointer",
+  };
 
-function Star({ onClick, full, onHoverIn, onHoverOut }) {
   return (
     <span
       role="button"
       style={starStyle}
       onClick={onClick}
       onMouseEnter={onHoverIn}
-      onMouseOut={onHoverOut}
+      onMouseLeave={onHoverOut}
     >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -96,7 +111,7 @@ function Star({ onClick, full, onHoverIn, onHoverOut }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
