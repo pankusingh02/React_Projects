583dd0305ab3239c9870bfbefbcc38d137867da4
pankusingh02 <pankajaswal2020@gmail.com>
2024-02-19 13:59:44 +0530

    Adding key prop so that state can be reset

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -74,6 +74,7 @@ export default function App() {
 
       {selectedFriend && (
         <FormSplitBill
+          key={selectedFriend.id}
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
         />
