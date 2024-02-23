4a36f2379be153e7b8f437deae63d3be34e0022f
pankusingh02 <pankajaswal2020@gmail.com>
2024-02-23 14:25:16 +0530

    using Async function inside useEffect hook

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -49,15 +49,25 @@ const tempWatchedData = [
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
 const key = "7b8c4e87";
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
 
-  fetch(`http://www.omdbapi.com/?apikey=${key}&s=inception`)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  const query = "inception";
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${key}&s=${query}`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+    fetchMovies();
+  }, []);
 
   return (
     <>
